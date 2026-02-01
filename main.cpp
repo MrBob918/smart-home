61fe06075c88816503784ba3b1c312c5786f934a
RRostislaW <megarosplay@gmail.com>
2026-02-02 01:35:59 +0300

    some change in CMakePresets, configure add_boost download, add comments to cmakelists, change in gitignore, and test main.cpp to test compile

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
+#include <boost/asio.hpp>
+#include <boost/beast/http.hpp>
 #include <iostream>
-int main() {
-    std::cout << "hello world!" << std::endl;
 
+namespace net  = boost::asio;
+namespace http = boost::beast::http;
+
+int main()
+{
+    net::io_context ioc;
+
+    http::request<http::string_body> req;
+    req.method(http::verb::get);
+    req.target("/");
+    req.version(11);
+
+    std::cout << "Beast is working, HTTP version = "
+              << req.version() << "\n";
 }
